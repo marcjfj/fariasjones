3d5e0b51f4b8846dc008fcbd11f6c570a63c087e
Marc <marcjfj@gmail.com>
2023-06-13 15:36:35 -0500

    term paddinh right

--- src/components/Terminal/Terminal.svelte
+++ src/components/Terminal/Terminal.svelte
@@ -25,7 +25,7 @@ const select = (type: string) => {
 </script>
 <div on:click={() => select('TERMINAL')} on:keydown={() => select('TERMINAL')} class={`rounded-lg min-h-[265px] overflow-hidden bg-gray-900 shadow-xl border ${borderClass}`}>
   <WindowHeader title={title} />
-  <div class="p-4">
+  <div class="p-4 pr-0">
     <div bind:this={termElement} class="term overflow-x-hidden"></div>
   </div>
 </div>
