5fceb5bce3f09aea498f8ce96e4c593ced7088f1
Marc <marcjfj@gmail.com>
2023-06-13 12:53:56 -0500

    overflow in term

--- src/components/Terminal/Terminal.svelte
+++ src/components/Terminal/Terminal.svelte
@@ -26,6 +26,6 @@ const select = (type: string) => {
 <div on:click={() => select('TERMINAL')} on:keydown={() => select('TERMINAL')} class={`rounded-lg min-h-[265px] overflow-hidden bg-gray-900 shadow-xl border ${borderClass}`}>
   <WindowHeader title={title} />
   <div class="p-4">
-    <div bind:this={termElement} class="term"></div>
+    <div bind:this={termElement} class="term overflow-x-hidden"></div>
   </div>
 </div>
