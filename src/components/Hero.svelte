4778fab93fa3f4c6dc14485fe64c337baa4303cf
Marc <marcjfj@gmail.com>
2023-06-12 23:40:11 -0500

    dont mount editor on mobile

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-import { onMount } from "svelte";
 import Editor from "./Editor/Editor.svelte";
 import Terminal from "./Terminal/Terminal.svelte";
 import data from "@data/hero";
