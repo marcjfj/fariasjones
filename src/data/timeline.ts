331f732af40ddee090a178443f1fbe367f605e34
Marc <marcjfj@gmail.com>
2024-07-24 18:53:45 -0700

    update timeline

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,4 +1,4 @@
-import vmlLogo from "../images/vml-logo.svg";
+import vmlLogo from "../images/VML_2024.svg";
 import llLogo from "../images/ll-logo.svg";
 
 export interface TimelineItem {
@@ -12,18 +12,27 @@ export interface TimelineItem {
 
 export default [
   {
+    date: "2024",
+    image: vmlLogo,
+    imageAlt: "VML",
+    title: "Associate Director, Technology",
+    description:
+      "Managing a team of developers and architects to deliver high-quality software solutions for clients. Leading technical strategy and innovation.",
+    tags: ["Technical Strategy", "Design Patterns", "Leadership", "Innovation"],
+  },
+  {
     date: "2023",
     image: vmlLogo,
-    imageAlt: "VMLY&R",
+    imageAlt: "VML",
     title: "Development Lead, Innovations",
     description:
       "Rapid development of prototypes and proof-of-concepts for creative new ideas and products. Creation of architectural documentation.",
-    tags: ["Typescript", "Vue", "Python", "AWS", "MongoDB", "Node.js"],
+    tags: ["Typescript", "Vue", "Python", "AWS", "MongoDB", "Node.js", "AI"],
   },
   {
     date: "2022",
     image: vmlLogo,
-    imageAlt: "VMLY&R",
+    imageAlt: "VML",
     title: "Development Lead, Cloud Services",
     description:
       "Development of cloud-based solutions for clients, including serverless applications, microservices, and data pipelines to be consumed by web and mobile platforms.",
@@ -32,7 +41,7 @@ export default [
   {
     date: "2020-2021",
     image: vmlLogo,
-    imageAlt: "VMLY&R",
+    imageAlt: "VML",
     title: "Software Developer, E-Commerce",
     description:
       "Development and maintenance of a custom enterprise e-commerce platform. Supporting junior developers and reviewing code.",
